b69af9d4ad17c36227bde9ad4f4e8ed238cf7969
Valeriy Harlamov <valera545@gmail.com>
2022-12-15 13:21:15 +0300

    client init

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
 	local: 'http://localhost:8080/',
-	vercel: 'https://lotus-test-server.vercel.app/',
+	vercel: 'https://lotus-test-server.vercel:8080/',
 }
 
 export default config.vercel
