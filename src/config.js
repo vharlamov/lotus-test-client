09ae4a84b0ee55b31d4426f5b45711edfc6a9c8d
Valeriy Harlamov <valera545@gmail.com>
2022-12-15 13:25:02 +0300

    change endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
 	local: 'http://localhost:8080/',
-	vercel: 'https://lotus-test-server.vercel:8080/',
+	vercel: 'https://lotus-test-server.vercel/',
 }
 
 export default config.vercel
