3113692158a2a22837378940c465058c8812ec90
Valeriy Harlamov <valera545@gmail.com>
2022-12-16 13:02:22 +0300

    change config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
 	local: 'http://localhost:8080/',
-	vercel: 'https://lotus-test-server.vercel.app/',
+	vercel: 'https://lotus-test-server.vercel.app:8080/',
 }
 
 export default config.vercel
